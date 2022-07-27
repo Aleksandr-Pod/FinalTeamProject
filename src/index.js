9152037be6faf4f4006e60bae5d2de9c9bacec2f
Aleksandr-Pod <stdfire@gmail.com>
2022-07-27 23:23:45 +0300

    baseURL + redux folder + index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+// import { store, persistor } from 'Redux/store';
+// import { PersistGate } from 'redux-persist/es/integration/react'
+import './index.css'; // можем убрать, если надо
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/final_team_project/">
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+        {/* <Provider store={store}> */}
+          <App />
+        {/* </Provider> */}
+      {/* </PersistGate> */}
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage'
+// импрорт редюсеров
+
+export const store = configureStore({
+    reducer: {
+
+    }
+})
+
