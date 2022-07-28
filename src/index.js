06a35e2d781e0cc985656fc3fc34bbe55e3d20d1
Aleksandr-Pod <stdfire@gmail.com>
2022-07-28 12:22:52 +0300

    some changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { App } from './components/App';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 // import { store, persistor } from 'Redux/store';
 // import { PersistGate } from 'redux-persist/es/integration/react'
 import './index.css'; // можем убрать, если надо
