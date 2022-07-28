6ee460af77224b874a19bdb91d4013a66c8d41aa
Aleksandr-Pod <stdfire@gmail.com>
2022-07-28 18:30:52 +0300

    redux-persist Store

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './components/App';
 import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux';
-// import { store, persistor } from 'Redux/store';
-// import { PersistGate } from 'redux-persist/es/integration/react'
-import './index.css'; // можем убрать, если надо
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/es/integration/react'
+// import './index.css'; // будем импортировать в каждый компонент
 import 'modern-normalize/modern-normalize.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/final_team_project/">
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-      {/* <Provider store={store}> */}
-      <App />
-      {/* </Provider> */}
-      {/* </PersistGate> */}
+      <PersistGate loading={null} persistor={persistor}>
+        <Provider store={store}>
+          <App />
+        </Provider> 
+      </PersistGate>
     </BrowserRouter>
   </React.StrictMode>,
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
-import storage from 'redux-persist/lib/storage'
-// импрорт редюсеров
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import authReducer from './authSlice';
 
-export const store = configureStore({
+
+const persistConfig = {
+  key: 'auth', storage
+}
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
     reducer: {
-
-    }
+        auth: persistedAuthReducer,
+    },
+    middleware: getDefaultMiddleware => getDefaultMiddleware({
+            serializableCheck: { ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER] }
+        })
 })
+
+const persistor = persistStore(store);
+export {persistor, store}
 
