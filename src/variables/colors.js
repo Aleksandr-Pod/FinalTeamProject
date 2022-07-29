c1bee200c497c57cd2b6ee5af21e8bcc94701a20
Tanya Kuchma <kuchma.tanya@ukr.net>
2022-07-29 18:21:59 +0400

    add colors.css

--- src/variables/colors.js
+++ src/variables/colors.js
@@ -1,16 +1,14 @@
 export const baseTheme = {
-	colors: {
-        white: "#ffffff",
-        black: "#000000",
-		turquoise: "#24cca7",
-        pink: "#ff6596",
-        lightpink: "#ffd8d0",
-        violet: "#c5baff",
-		purple: "#4a56e2",
-		blue: "#6E78E8",
-		grey: "#bdbdbd",
-		lightGray: "#e0e0e0",
-        lightGray2: "#A6A6A6",
-        background: "e7eaf2"
-	}	
+    white: "#ffffff",
+    black: "#000000",
+    turquoise: "#24cca7",
+    pink: "#ff6596",
+    lightpink: "#ffd8d0",
+    violet: "#c5baff",
+    blue: "#4a56e2",
+    lightBlue: "#6e78e8",
+    grey: "#bdbdbd",
+    lightGrey: "#e0e0e0",
+    darkGrey: "#a6a6a6",
+    background: "#e7eaf2"		
 };
