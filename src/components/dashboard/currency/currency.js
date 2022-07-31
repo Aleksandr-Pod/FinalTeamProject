7b9263718f995c1a20690f38e96c2537a639283c
Denis Fedash <0501054104d@gmail.com>
2022-07-31 19:01:02 +0300

    add localstorage to currency

--- src/components/dashboard/currency/currency.js
+++ src/components/dashboard/currency/currency.js
@@ -1,10 +1,15 @@
 import { useEffect, useState } from 'react';
 import css from './currency.module.css';
-import { getCurrency } from '../../../api/currencyAPI';
+import propTypes from 'prop-types';
 import { nanoid } from 'nanoid';
+import { getCurrency } from '../../../api/currencyAPI';
 
 export const Currency = () => {
-  const [currencies, setCurrencies] = useState(null);
+  const [currencies, setCurrencies] = useState([]);
+
+  const time = 60 * 60 * 3000;
+  const currentTime = new Date().getTime();
+
   useEffect(() => {
     const getApi = async () => {
       await getCurrency().then(data => {
@@ -19,30 +24,70 @@ export const Currency = () => {
           return currenciesArray.push(currenc);
         });
         setCurrencies(currenciesArray);
-        console.log(currenciesArray);
+        localStorage.setItem(
+          'currency',
+          JSON.stringify({
+            currentTime: currentTime,
+            currencies: currenciesArray,
+          }),
+        );
       });
     };
     getApi();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEffect(() => {
+    if (localStorage.getItem('currency') !== null) {
+      const deadline =
+        JSON.parse(localStorage.getItem('currency')).currentTime + time;
+      const endOfDeadline = deadline - time;
+      if (endOfDeadline < 0) {
+        getCurrency();
+      } else {
+        const oldData = JSON.parse(localStorage.getItem('currency')).currencies;
+        setCurrencies(oldData);
+      }
+    } else {
+      getCurrency();
+      localStorage.setItem('currency', JSON.stringify(currencies));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <table className={css.currency}>
-      <thead>
-        <tr className={css.currencyHead}>
-          <th>Currency</th>
-          <th>Purchase</th>
-          <th>Sale</th>
-        </tr>
-      </thead>
-      <tbody className={css.currencyBody}>
-        {currencies &&
-          currencies.map(({ id, ccy, buy, sale }) => (
-            <tr key={id} className={css.currencyInfo}>
-              <td>{ccy}</td>
-              <td>{Number(buy).toFixed(2)}</td>
-              <td>{Number(sale).toFixed(2)}</td>
-            </tr>
-          ))}
-      </tbody>
-    </table>
+    <div className={css.currencyContainer}>
+      <table className={css.currency}>
+        <thead>
+          <tr className={css.currencyHead}>
+            <th>Currency</th>
+            <th>Purchase</th>
+            <th>Sale</th>
+          </tr>
+        </thead>
+        <tbody className={css.currencyBody}>
+          {currencies &&
+            currencies.map(({ ccy, buy, sale }) => (
+              <tr key={nanoid()} className={css.currencyInfo}>
+                <td>{ccy}</td>
+                <td>{Number(buy).toFixed(2)}</td>
+                <td>{Number(sale).toFixed(2)}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+      <div className={css.vector}></div>
+    </div>
   );
+};
+
+Currency.prototype = {
+  currencies: propTypes.arrayOf(
+    propTypes.shape({
+      id: propTypes.string,
+      ccy: propTypes.string,
+      buy: propTypes.string,
+      sale: propTypes.number,
+    }),
+  ).isRequired,
 };
