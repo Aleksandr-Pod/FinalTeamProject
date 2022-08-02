2c372995e22b62661e5708f3da6c28ecf7f7c1c9
Glasgalas <alex.starichenko@icloud.com>
2022-08-02 18:08:37 +0300

    add fetch categories

--- src/redux/store.js
+++ src/redux/store.js
@@ -13,6 +13,7 @@ import storage from 'redux-persist/lib/storage';
 import authReducer from './authSlice';
 import currencyReducer from './curerncySlice';
 import dataReducer from './dataSlice';
+import categoriesReducer from './categoriesSlice';
 
 const persistConfig = {
   key: 'auth',
@@ -20,20 +21,31 @@ const persistConfig = {
 };
 const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 
+const persistedCurrencyReducer = persistReducer(
+  { key: 'currencies', storage },
+  currencyReducer,
+);
 
-const persistedCurrencyReducer = persistReducer({ key: 'currencies', storage }, currencyReducer);
+const persistCategories = persistReducer(
+  { key: 'categories', storage },
+  categoriesReducer,
+);
+
 const store = configureStore({
   reducer: {
     auth: persistedAuthReducer,
     currency: persistedCurrencyReducer,
-    data: dataReducer
+    data: dataReducer,
+    categories: persistCategories,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
+  middleware: getDefaultMiddleware => [
+    ...getDefaultMiddleware({
       serializableCheck: {
         ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     }),
+  ],
+  devToolsls: process.env.NODE_ENV !== 'production',
 });
 
 const persistor = persistStore(store);
