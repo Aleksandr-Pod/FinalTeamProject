3061ceb81cd1e89fc8cf1201dc29500cb94e3f56
Glasgalas <alex.starichenko@icloud.com>
2022-08-03 16:00:57 +0300

    edit adding trancs

--- src/components/modal/modalAddTransaction/modalAddTransaction.js
+++ src/components/modal/modalAddTransaction/modalAddTransaction.js
@@ -50,14 +50,14 @@ export default function ModalAddTransaction({ showModal, setShowModal }) {
 
   const handleInputChange = e => {
     const { name, value } = e.target;
-    // const id = e.target.options[e.target.selectedIndex].id;
+    const ind = e.target.selectedIndex;
     switch (name) {
       case 'amount':
         setAmount(value);
         break;
       case 'category':
         setCategory(value);
-        setCategoryId(1);
+        setCategoryId(e.target.options[ind].id);
         break;
       case 'date':
         setDate(value);
@@ -72,18 +72,26 @@ export default function ModalAddTransaction({ showModal, setShowModal }) {
 
   const handleSubmit = () => {
     const newDate = calcDate();
-    const data = { amount, isIncome: type, date: newDate, category, categoryId: "1", comment };
-    console.log("submitted data", data);
+    const amountNum = Number(amount).toFixed(2);
+    const data = {
+      amount: amountNum,
+      isIncome: type,
+      date: newDate,
+      category,
+      categoryId,
+      comment,
+    };
+    console.log('submitted data', data);
     dispatch(transactionOperations.addTransaction(data));
     setShowModal(false);
-  }
-  function calcDate () {
+  };
+  function calcDate() {
     const today = new Date();
     let day = today.getDate();
     if (day < 10) day = `0${day}`;
-    let month = today.getMonth()+1;
+    let month = today.getMonth() + 1;
     if (month < 10) month = `0${month}`;
-    return `${day}-${month}-${today.getFullYear()}`
+    return `${month}-${day}-${today.getFullYear()}`;
   }
 
   return (
@@ -122,32 +130,33 @@ export default function ModalAddTransaction({ showModal, setShowModal }) {
 
                 <span className={styles.outcome}>Expense</span>
               </div>
-              {!type && (
-                <select
-                  name="category"
-                  className={styles.select}
-                  value={category}
-                  onChange={handleInputChange}
-                  // id={categoryId}
-                >
-                  <option>Select a category</option>
-                  {type
-                    ? categories.income.map(el => {
-                        return (
-                          <option key={el.id} value={el.name} id={el.id}>
-                            {el.name}
-                          </option>
-                        );
-                      })
-                    : categories.expense.map(el => {
-                        return (
-                          <option key={el.id} value={el.name} id={el.id}>
-                            {el.name}
-                          </option>
-                        );
-                      })}
-                </select>
-              )}
+
+              <select
+                name="category"
+                className={styles.select}
+                value={category}
+                onChange={handleInputChange}
+                id={categoryId}
+                placeholder={'Select a category'}
+              >
+                {/* <option>Select a category</option> */}
+                {type
+                  ? categories.income.map(el => {
+                      return (
+                        <option key={el.id} value={el.name} id={el.id}>
+                          {el.name}
+                        </option>
+                      );
+                    })
+                  : categories.expense.map(el => {
+                      return (
+                        <option key={el.id} value={el.name} id={el.id}>
+                          {el.name}
+                        </option>
+                      );
+                    })}
+              </select>
+
               <div className={styles.amount}>
                 <input
                   className={styles.inputAmount}
@@ -177,7 +186,11 @@ export default function ModalAddTransaction({ showModal, setShowModal }) {
                 onChange={handleInputChange}
               />
               <div className={styles.buttons}>
-                <button className={styles.addBtn} type="submit" onClick={handleSubmit}>
+                <button
+                  className={styles.addBtn}
+                  type="submit"
+                  onClick={handleSubmit}
+                >
                   Add
                 </button>
                 <button
