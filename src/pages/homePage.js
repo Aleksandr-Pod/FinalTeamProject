e7f2420e54fb6ef2f3cf74e9e50e03eecb4e7fd7
Oleksii Losiev <75750709+alexlex7@users.noreply.github.com>
2022-08-05 23:06:23 +0300

    added tostify on home page

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,4 +1,4 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import Media from 'react-media';
 // components
 import {
@@ -10,8 +10,18 @@ import { NavMenu } from '../components/dashboard/navMenu/navMenu';
 import { Currency } from '../components/dashboard/currency/currency';
 import { Ballance } from '../components/dashboard/ballance/ballance';
 import styles from './homePage.module.css';
+import { ToastContainer, toast } from 'react-toastify';
+import { useSelector } from 'react-redux';
 
 export const HomePage = () => {
+  const { error } = useSelector(state => state.transactions);
+
+  useEffect(() => {
+    if (error) {
+      toast(error);
+    }
+  }, [error]);
+
   return (
     <>
       <Media
@@ -25,6 +35,7 @@ export const HomePage = () => {
           <Fragment>
             {matches.small && (
               <>
+                <ToastContainer />
                 <NavMenuMobile />
                 <Ballance />
                 <div className={styles.containerTable}>
@@ -34,6 +45,7 @@ export const HomePage = () => {
             )}
             {matches.medium && (
               <>
+                <ToastContainer />
                 <div className={styles.currency}>
                   <div className={styles.nav_ballance}>
                     <NavMenu />
@@ -48,6 +60,7 @@ export const HomePage = () => {
             )}
             {matches.large && (
               <>
+                <ToastContainer />
                 <div className={styles.largeSize}>
                   <div className={styles.leftSize}>
                     <NavMenu />
