945f34180485db413b9400ed676075b59e945dbf
Oleksii Losiev <75750709+alexlex7@users.noreply.github.com>
2022-08-05 22:38:40 +0300

    added spinner to logout modal

--- src/components/modalLogout/modalLogout.js
+++ src/components/modalLogout/modalLogout.js
@@ -1,51 +1,64 @@
 import styles from './modalLogout.module.css';
+import { useSelector } from 'react-redux';
 import { createPortal } from 'react-dom';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import authOperations from '../../redux/auth/authOperations';
+import { Spinner } from '../spinner/spinner';
 
 const modalRoot = document.getElementById('modal-root');
 
-
 export default function ModalLogout({ closeModalLogout }) {
-    const dispatch = useDispatch();
-  
-    const logOut = () => {
-        dispatch(authOperations.logOut());
-        closeModalLogout();
-    };
-    
-    const onBackdropClick = (e) => {
-        if (e.target === e.currentTarget) {
-            closeModalLogout();
-        }
-    };
-    
-    const closeModalByEsc = (e) => {
-        if (e.code === 'Escape') {
-            closeModalLogout();
-        }
-    };
-    
-    useEffect(() => {
-        window.addEventListener('keydown', closeModalByEsc);
-        return () => {
-            window.removeEventListener('keydown', closeModalByEsc);
-        };
-    });
+  const dispatch = useDispatch();
+  const { isLoading } = useSelector(state => state.auth);
+  console.log(isLoading);
 
-    return createPortal(
-        <>
-            <div className={styles.logoutWrapper}  onClick={onBackdropClick}>
-                <div className={styles.logoutContent}>
-                    <h2 className={styles.logoutTitle}>Log out</h2>
-                    <p className={styles.logoutText}>Are you sure you want to log out?</p>
-                    <div className={styles.logoutButtons}>
-                        <button type='button' className={styles.logoutBtn} onClick={logOut}>Log Out</button>
-                        <button type='button' className={styles.logoutCancelBtn} onClick={()=> closeModalLogout()}>Cancel</button>
-                    </div>
-                </div>
-            </div>
-        </>, modalRoot
-    )
+  const logOut = () => {
+    dispatch(authOperations.logOut());
+    // closeModalLogout();
+  };
+
+  const onBackdropClick = e => {
+    if (e.target === e.currentTarget) {
+      closeModalLogout();
+    }
+  };
+
+  const closeModalByEsc = e => {
+    if (e.code === 'Escape') {
+      closeModalLogout();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', closeModalByEsc);
+    return () => {
+      window.removeEventListener('keydown', closeModalByEsc);
+    };
+  });
+
+  return createPortal(
+    <>
+      <div className={styles.logoutWrapper} onClick={onBackdropClick}>
+        <div className={styles.logoutContent}>
+          {isLoading && <Spinner />}
+          <h2 className={styles.logoutTitle}>Log out</h2>
+          <p className={styles.logoutText}>Are you sure you want to log out?</p>
+          <div className={styles.logoutButtons}>
+            <button type="button" className={styles.logoutBtn} onClick={logOut}>
+              Log Out
+            </button>
+            <button
+              type="button"
+              className={styles.logoutCancelBtn}
+              onClick={() => closeModalLogout()}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </>,
+    modalRoot,
+  );
 }
