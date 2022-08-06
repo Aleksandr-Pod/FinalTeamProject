ff716715d37fb7e6894f8f0ca9bb4b9f776e3d7e
Aleksandr-Pod <stdfire@gmail.com>
2022-08-06 15:36:07 +0300

    lazy loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,23 @@
-import { useEffect, lazy, Suspense } from 'react';
+import { useEffect, lazy } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 // Components
-import { PrivateRoute } from './privateRoute';
-import { Dashboard } from '../pages/dashboard';
-import { RedirectRoute } from './redirectRoute';
-import { PageNotFound } from './pageNotFound/pageNotFound';
+import PrivateRoute from './privateRoute';
+import RedirectRoute from './redirectRoute'; // ругается, если lazy import
+// import PageNotFound from './pageNotFound/pageNotFound';
 import { RegisterPage } from '../pages/registerPage';
 import { LoginPage } from '../pages/loginPage';
+import Dashboard from '../pages/dashboard';
+
 import authOperations from '../redux/auth/authOperations';
 import transactionsOperations from '../redux/transactions/transactionOperations';
 import { fetchStatistics } from '../redux/statistics/statisticsOperations';
 import { ToastContainer } from 'react-toastify';
+
+// const PrivateRoute = lazy(() => import('./privateRoute'));
+const PageNotFound = lazy(() => import('../pages/pageNotFound'));
+// const RedirectRoute = lazy(() => import('./redirectRoute'));
+// const Dashboard = lazy(() => import ('../pages/dashboard'));
 
 export const App = () => {
   const { isLogged } = useSelector(state => state.auth);
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,15 +1,24 @@
-import Header from '../components/dashboard/header/header';
+import { lazy } from 'react';
 import { useParams } from 'react-router-dom';
+// components
 import { HomePage } from './homePage';
 import { CurrencyPage } from './currencyPage';
 import { StatPage } from './statPage';
-import { PageNotFound } from '../components/pageNotFound/pageNotFound';
+import PageNotFound from './pageNotFound';
+import Header from '../components/dashboard/header/header';
 import PurpleEllipse from '../images/Ellipse1.svg';
 import PeachEllipse from '../images/Ellipse2.svg';
 import styles from './styles.module.css';
 import Modal from '../components/modal/modal';
 
-export const Dashboard = () => {
+//          не удалять !
+//          после переименования файлов снять коммент.
+// const HomePage = lazy(() => import('./homePage'));
+// const CurrencyPage = lazy(() => import('./currencyPage'));
+// const StatPage = lazy(() => import ('./statPage'));
+// const PageNotFound = lazy(() => import('./pageNotFound'));
+
+const Dashboard = () => {
   console.log('Dashboard');
 
   const { activeBtn } = useParams();
@@ -27,12 +36,13 @@ export const Dashboard = () => {
       <img className={styles.peach} src={PeachEllipse} alt="logo" />
       <div className={styles.dash}>
         <Header />
-        <Modal/>
+        <Modal />
         {activeBtn === 'home' && <HomePage />}
         {activeBtn === 'currency' && <CurrencyPage />}
         {activeBtn === 'diagram' && <StatPage />}
       </div>
-      <Modal/>
+      <Modal />
     </>
   );
 };
+export default Dashboard;
