f0f5c6a4d6717d07f22d429e02eaf43f99395d5c
mandrykk <mandrykk22@gmail.com>
2022-08-07 20:19:59 +0300

    style improvements

--- src/components/dashboard/header/header.js
+++ src/components/dashboard/header/header.js
@@ -13,7 +13,7 @@ export default function Header() {
   const { user } = useSelector(state => state.auth);
 
   const toggleLogin = () => {
-    setShowModal(!showModal)
+    setShowModal(!showModal);
   };
 
   return (
@@ -30,23 +30,20 @@ export default function Header() {
         <div className={styles.secondWrapper}>
           <p className={styles.user}>{user.name}</p>
           <div className={styles.vl}></div>
-          <div className={styles.logOut }>
+          <div className={styles.logOut}>
             <button className={styles.exitButton} onClick={toggleLogin}>
               <svg className={styles.exit} width="18" height="18">
                 <use href={`${sprite}#icon-exit`}></use>
               </svg>
             </button>
             <button
-               onClick={toggleLogin}
+              onClick={toggleLogin}
               type="button"
               className={styles.button}
-              >
+            >
               Exit
             </button>
-            {showModal && (
-              <ModalLogout
-                closeModalLogout={toggleLogin}/>
-            )}
+            {showModal && <ModalLogout closeModalLogout={toggleLogin} />}
           </div>
         </div>
       </header>
