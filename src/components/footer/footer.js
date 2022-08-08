f76b24c44a8ffdd84a9c201cbf20091d42d23e9c
Tanya Kuchma <kuchma.tanya@ukr.net>
2022-08-08 17:40:59 +0400

    add team's modal

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,6 +1,15 @@
+import { useState } from 'react';
 import styles from './footer.module.css';
 
+import ModalTeam from '../modalTeam/modalTeam';
+
 export default function Footer() {
+    const [showModal, setShowModal] = useState(false);
+
+    const toggleModalTeam = () => {
+        setShowModal(!showModal);
+    };
+
     return (
         <footer className={styles.footer}>
             <div className={styles.container}>
@@ -10,9 +19,10 @@ export default function Footer() {
                 </div>
                 <div className={styles.bottom}>
                     <p className={styles.text}> Developed by </p>
-                    <button className={styles.btn} type="button">
+                    <button className={styles.btn} type="button" onClick={toggleModalTeam}>
                         <span className={styles.team}>team</span>
                     </button>
+                    {showModal && <ModalTeam closeModalTeam={toggleModalTeam} />}
                 </div>
             </div>
         </footer>
