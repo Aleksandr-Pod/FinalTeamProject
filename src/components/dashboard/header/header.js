71cbe259a4e0e22edfcb96344153891f6e130dc8
Aleksandr-Pod <stdfire@gmail.com>
2022-08-08 17:34:06 +0300

    loginPage RegPage height=1024

--- src/components/dashboard/header/header.js
+++ src/components/dashboard/header/header.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import styles from './header.module.css';
 import sprite from '../../../images/sprite.svg';
