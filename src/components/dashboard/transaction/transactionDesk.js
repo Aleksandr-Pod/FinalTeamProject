fbe120f05ddd24f3473d1d0b7335e96985c312a6
Aleksandr Podmazko <stdfire@gmail.com>
2022-08-20 09:42:55 +0300

    edit-delete menu

--- src/components/dashboard/transaction/transactionDesk.js
+++ src/components/dashboard/transaction/transactionDesk.js
@@ -1,11 +1,43 @@
+import { useState, useEffect, useCallback } from 'react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import styles from './transactionDesk.module.css';
 import { TransactionTableDesk } from './transactionTableDesk';
 
 export const TransactionDesk = () => {
+  const [showTableModal, setShowTableModal] = useState(false);
+  console.log('showTableModal', showTableModal);
   const { t } = useTranslation();
   const { transactions } = useSelector(state => state.transactions);
+  let transId = 0;
+
+  const keyPress = useCallback(
+    e => {
+      if (e.key === 'Escape' && showTableModal) {
+        setShowTableModal(false);
+      }
+    },
+    [setShowTableModal, showTableModal],
+  );
+
+  useEffect(() => {
+    document.addEventListener('keydown', keyPress);
+    return () => document.removeEventListener('keydown', keyPress);
+  }, [keyPress]);
+
+  const handleClick = id => {
+    console.log('handleClick _id', id);
+    setShowTableModal(true);
+  };
+
+  const editRecord = id => {
+    console.log('Edit record id:', id);
+  };
+
+  const deleteRecord = id => {
+    console.log('Delete record id:', id);
+  };
+
   return (
     <>
       {transactions.length === 0 && (
@@ -14,53 +46,64 @@ export const TransactionDesk = () => {
         </>
       )}
       {transactions.length > 0 && (
-        <div className={styles.tableWrapper}>
-          <table className={styles.transactionTable}>
-            <thead>
-              <tr className={styles.headString}>
-                <th className={styles.headDate}>{t('transactions.date')}</th>
-                <th>{t('transactions.type')}</th>
-                <th className={styles.headCategory}>
-                  {t('transactions.category')}
-                </th>
-                <th className={styles.headComment}>
-                  {t('transactions.comment')}
-                </th>
-                <th className={styles.headSum}>{t('transactions.sum')}</th>
-                <th className={styles.headBalance}>
-                  {t('transactions.balance')}
-                </th>
-              </tr>
-            </thead>
-            <tbody className="">
-              {transactions.map(
-                ({
-                  _id,
-                  date,
-                  isIncome,
-                  category,
-                  comment,
-                  amount,
-                  balance,
-                }) => (
-                  <TransactionTableDesk
-                    key={_id}
-                    date={date}
-                    isIncome={isIncome}
-                    category={category}
-                    comment={comment}
-                    sum={amount
-                      .toString()
-                      .replace(/\B(?=(\d{3})+(?!\d))/g, ' ')}
-                    balance={balance
-                      .toString()
-                      .replace(/\B(?=(\d{3})+(?!\d))/g, ' ')}
-                  />
-                ),
-              )}
-            </tbody>
-          </table>
-        </div>
+        <>
+          <div className={styles.tableWrapper}>
+            <table className={styles.transactionTable}>
+              <thead>
+                <tr className={styles.headString}>
+                  <th className={styles.headDate}>{t('transactions.date')}</th>
+                  <th>{t('transactions.type')}</th>
+                  <th className={styles.headCategory}>
+                    {t('transactions.category')}
+                  </th>
+                  <th className={styles.headComment}>
+                    {t('transactions.comment')}
+                  </th>
+                  <th className={styles.headSum}>{t('transactions.sum')}</th>
+                  <th className={styles.headBalance}>
+                    {t('transactions.balance')}
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="">
+                {transactions.map(
+                  ({
+                    _id,
+                    date,
+                    isIncome,
+                    category,
+                    comment,
+                    amount,
+                    balance,
+                  }) => (
+                    <TransactionTableDesk
+                      key={_id}
+                      handleClick={handleClick}
+                      date={date}
+                      isIncome={isIncome}
+                      category={category}
+                      comment={comment}
+                      sum={amount
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ' ')}
+                      balance={balance
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ' ')}
+                    />
+                  ),
+                )}
+              </tbody>
+            </table>
+          </div>
+          {showTableModal && (
+            <div className={styles.layout}>
+              <button className={styles.firstBtn} onClick={editRecord}>
+                EDIT
+              </button>
+              <button onClick={deleteRecord}>DELETE</button>
+            </div>
+          )}
+        </>
       )}
     </>
   );
--- src/components/dashboard/transaction/transactionTableDesk.js
+++ src/components/dashboard/transaction/transactionTableDesk.js
@@ -1,6 +1,8 @@
 import EllipsisText from 'react-ellipsis-text/lib/components/EllipsisText';
 import styles from './transactionDesk.module.css';
 export const TransactionTableDesk = ({
+  handleClick,
+  id,
   date,
   isIncome,
   category,
@@ -9,19 +11,17 @@ export const TransactionTableDesk = ({
   balance,
 }) => {
   return (
-    <>
-      <tr className={styles.bodyString}>
-        <td className={styles.date}> {date} </td>
-        <td>{isIncome ? '+' : '-'}</td>
-        <td className={styles.category}>{category}</td>
-        <td className={styles.comment}>
-          <EllipsisText text={comment} length={20} />
-        </td>
-        <td className={`${isIncome ? styles.incomeSum : styles.expensSum}`}>
-          {sum}{' '}
-        </td>
-        <td className={styles.balance}>{balance} </td>
-      </tr>
-    </>
+    <tr className={styles.bodyString} onClick={() => handleClick(id)}>
+      <td className={styles.date}> {date} </td>
+      <td>{isIncome ? '+' : '-'}</td>
+      <td className={styles.category}>{category}</td>
+      <td className={styles.comment}>
+        <EllipsisText text={comment} length={20} />
+      </td>
+      <td className={`${isIncome ? styles.incomeSum : styles.expensSum}`}>
+        {sum}{' '}
+      </td>
+      <td className={styles.balance}>{balance} </td>
+    </tr>
   );
 };
